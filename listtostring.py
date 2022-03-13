a09822c85d22022e3d37cdeab76f1d59e1393a49
Pravallika <96050882+prava-coder@users.noreply.github.com>
2022-03-13 11:37:45 +0530

    Add files via upload

---
+++ listtostring.py
@@ -0,0 +1,7 @@
+#pthon program to convert a list in to a  string
+#creating a list
+list1=['I','am','very','good','girl']
+#to convert list to string
+listtostring=''.join(list1)
+print(listtostring)
+print(type(listtostring))
